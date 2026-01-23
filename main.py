428f9331c4724334fd46cf2df9d8949981ff5154
Shooya1 <shooyabrian@gmail.com>
2026-01-23 20:47:33 +0200

    Serarch by name function and minor big fixes

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ students = load_students()
 def generate_id():
     if not students:
         return 1
-    return students[-1]["id"] + 1
+    return max(student["id"] for student in students) + 1
+
 
 # Add Student
 
@@ -47,6 +48,24 @@ def view_students():
             f'{student["field"]}'
         )
 
+# Search Student
+
+def search_student():
+    query = input("Enter name to search: ").lower()
+   
+    for student in students:
+        if student["name"].lower() == query:
+         print(
+            f'ID: {student["id"]} | '
+            f'{student["name"]}, '
+            f'{student["age"]} yrs, '
+            f'{student["field"]}'
+         )
+         return
+         
+        
+    print("Student not found.")
+
 # Update Student
 
 def update_student():
@@ -83,14 +102,20 @@ def update_student():
     print("Student not found.")
 
 # Delete Student
+def get_valid_id(prompt):
+    while True:
+        try:
+            return int(input(prompt))
+        except ValueError:
+            print("Please enter a valid number.")
 
 def delete_student():
-    student_id = int(input("Enter student ID to update: "))
+    student_id = get_valid_id("Enter student ID to delete: ")
 
     for student in students:
         if student["id"] == student_id:
             students.remove(student)
-            save_students(student)
+            save_students(students)
             print("Student deleted.")
             return
     print("Student not found.")
@@ -100,23 +125,26 @@ def delete_student():
 
 while True:
    print("\nStudent Record Manager")
-   print("1. Add Student")
-   print("2. View Students")
-   print("3. Update Student")
-   print("4. Delete Student")
-   print("5. Exit")
+   print("1. Add a Student")
+   print("2. Search for a Student")
+   print("3. View Students")
+   print("4. Update a Student")
+   print("5. Delete a Student")
+   print("6. Exit")
 
    choice = input("Choose an option: ")
 
    if choice == "1":
        add_student()
    elif choice == "2":
-       view_students()
+       search_student()
    elif choice == "3":
-       update_student()
+       view_students()
    elif choice == "4":
-       delete_student()
+       update_student()
    elif choice == "5":
+       delete_student()
+   elif choice == "6":
        print("Goodbye.")
        break
    else:
